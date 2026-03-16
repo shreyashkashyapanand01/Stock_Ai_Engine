7efdf4cd63ab38948ad07b0d7bbaccd7dfd3113e
shreyashkashyapanand01 <shreyashkashyapanand01@gmail.com>
2026-03-16 12:37:26 +0530

    fixed stock not found error

--- app/tools/fundamental_tool.py
+++ app/tools/fundamental_tool.py
@@ -1,8 +1,9 @@
 import yfinance as yf
-
+from app.tools.market_data_tool import get_stock_name
 
 def fetch_fundamental_data(symbol: str):
 
+    symbol=get_stock_name(symbol) #edited
     if not symbol.endswith(".NS"):
         symbol = symbol + ".NS"
 
