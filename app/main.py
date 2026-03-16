a42ded6bfa08b942fb921073b48a15d7003c7416
shreyashkashyapanand01 <shreyashkashyapanand01@gmail.com>
2026-03-16 21:55:57 +0530

    updated main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from app.api.stock_api import router as stock_router
 
-app = FastAPI()
+from app.api.stock_api import router as stock_router
+from app.api.scan_api import router as scan_router
+
+
+app = FastAPI(
+    title="AI Stock Intelligence Engine"
+)
 
 app.include_router(stock_router)
-
-@app.get("/")
-def home():
-    return {"message": "AI Engine Running"}
+app.include_router(scan_router)
